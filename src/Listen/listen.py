a991b5d491266f74d02b18bbba6ba10ab31bf352
Aditya Pratap Singh <adityaontech@gmail.com>
2025-06-04 18:50:12 +0530

    listening to user and generating string

---
+++ src/Listen/listen.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+def listen():
+	recognizer = sr.Recognizer()
+
+	with sr.Microphone() as source:
+		
+		try:
+			print("Speak...")
+			audio = recognizer.listen(source, timeout=10)
+
+			text = recognizer.recognize_google(audio)
+
+			return text
+
+		except Exception as e:
+			return f"some exception occured : {e}"
